aae12e2bca6f673f0cbf54f382a9dd6dad9241ab
Joshua Manigault <norcaz919@gmail.com>
2026-02-26 17:30:19 -0700

    Add another Plus One solution

---
+++ 66-plus-one/66-solution2.py
@@ -0,0 +1,10 @@
+class Solution:
+    def plusOne(self, digits: list) -> list:
+        for i in range(len(digits) - 1, -1, -1):
+            if (digits[i] + 1) != 10:
+                digits[i] += 1
+                return digits
+            digits[i] = 0
+        
+
+        return [1] + digits
